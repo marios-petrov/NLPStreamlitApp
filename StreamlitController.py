c5e09736cb3062e8357394dc0a5790ad6d98bd49
marios-petrov <marios.petrov23@ncf.edu>
2022-12-06 11:44:42 -0500

    Streamlit Summary Implementation

--- StreamlitController.py
+++ StreamlitController.py
@@ -1,20 +1,42 @@
 import streamlit as st
-import pandas as pd
-import nltk as nlp
-import os
-#import WikiBERT as bert
-#import WikiScrapper as ws
+import nltk
+from summarizer import Summarizer,TransformerSummarizer
 
-st.title("BERT vs WikipediaAPI")
+def BERT_Summarizer(input_text):
+    bertModel = Summarizer()
+    bertSummary = ''.join(bertModel(input_text,min_length = 60))
+    return bertSummary
+def GPT_Summarizer(input_text):
+    gptModel = TransformerSummarizer(transformer_type="GPT2",transformer_model_key="gpt2-medium")
+    gptSummary = ''.join(gptModel(input_text,min_length=60))
+    return gptSummary
+def XLNet_Summarizer(input_text):
+    xlnetModel = TransformerSummarizer(transformer_type="XLNet",transformer_model_key="xlnet-base-cased")
+    xlnetSummary = ''.join(xlnetModel(input_text, min_length=60))
+    return xlnetSummary
 
-input_type = st.radio("Input Type: ", ["URL", "Raw Text"])
-st.markdown("<h3 style='text-align: center;'>Input</h3>", unsafe_allow_html=True)
+def main():
+    st.title ("NLP text Processing and Summarization")
+    activity1 = ["Summarize","Text Preprocessing"]
+    choice = st.sidebar.selectbox("Select Function",activity1)
+    if choice == 'summarizer':
+        st.subheader("summarizer mode")
+        raw_text = st.text_area("Enter Text Here")
+        summarizer_choice = st.selectbox("Summary Choice", ["XLNet", "GPT", "BERT"])
+        if st.button('Summarizer'):
+            if summarizer_choice == "XLNet":
+                summarizer_result = XLNet_Summarizer(raw_text)
+                st.write(summarizer_result)
+            if summarizer_choice == "GPT":
+                summarizer_result = GPT_Summarizer(raw_text)
+                st.write(summarizer_result)
+            if summarizer_choice == "BERT":
+                summarizer_result = BERT_Summarizer(raw_text)
+                st.write(summarizer_result)
 
-if input_type == "Raw Text":
-    with open("raw_data/input.txt") as f:
-        sample_text = f.read()
-    text = st.text_area("", sample_text, 200)
-else:
-    url = st.text_input("")
 
 
+
+
+if __name__ == '__main__':
+    main()
